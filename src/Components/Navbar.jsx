fe3a57c960e90ddccfdc824c9173558064cebc12
mojeshdev <mojeshlam823@email.com>
2025-08-21 11:09:01 +0530

    Update Home and Navbar components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
         <div className="hidden lg:flex fixed right-6 top-1/2 -translate-y-1/2 flex-col items-end space-y-6 z-50">
         {navItems.map(({ to, icon: Icon, label }) => (
             <NavLink key={to} to={to}>
-            <div className="group relative w-14 h-14 bg-yellow-500 rounded-full flex items-center justify-center shadow-lg transition-all duration-300 overflow-hidden hover:w-44 hover:justify-end pr-4">
+            <div className="group relative w-14 h-14  bg-yellow-500 rounded-full flex items-center justify-center shadow-lg transition-all duration-300 overflow-hidden hover:w-44 hover:justify-end pr-4">
               {/* Label appears on hover */}
                 <span className="absolute right-14 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white font-bold whitespace-nowrap">
                 {label}
